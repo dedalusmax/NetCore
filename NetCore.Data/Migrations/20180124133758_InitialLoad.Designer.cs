e474b7b5b2aa0d665bf27202fc095adebb6d7ce7
dedalusmax <dedalus123@gmail.com>
2018-01-24 14:39:33 +0100

    migrations available

--- NetCore.Data/Migrations/20180124133758_InitialLoad.Designer.cs
+++ NetCore.Data/Migrations/20180124133758_InitialLoad.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace NetCore.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180124133758_InitialLoad")]
+    partial class InitialLoad
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- NetCore.Data/Migrations/20180124133758_InitialLoad.cs
+++ NetCore.Data/Migrations/20180124133758_InitialLoad.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
 
 namespace NetCore.Data.Migrations
 {
-    public partial class CreateIdentitySchema : Migration
+    public partial class InitialLoad : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+
         }
     }
-
 }
